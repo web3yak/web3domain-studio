e9a5c8acea40b15d981cd078ebf28f6349be6b0d
gupta977 <navneet@odude.com>
2023-07-29 19:11:02 +0545

    1.0

--- src/pages/domain.tsx
+++ src/pages/domain.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { useAccount, useSignMessage, useContractRead, erc721ABI } from "wagmi";
 import {
   Box,
@@ -13,55 +13,44 @@ import {
   Textarea,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { verifyMessage } from "ethers/lib/utils";
 import { SignMessageArgs } from "@wagmi/core";
 import { NextSeo } from "next-seo";
-import abiFile from '../web3domain.json';
-
-const CONTRACT_ADDRESS = '0x7D853F9A29b3c317773A461ed87F54cdDa44B0e0';
-
-const JellyBotsChecker = ({ address }: { address: string }) => {
-  const { data } = useContractRead({
-    addressOrName: CONTRACT_ADDRESS,
-    contractInterface: erc721ABI,
-    functionName: "balanceOf",
-    args: address,
-  });
-
-  const [hasJellyBot, setHasJellyBot] = useState(false);
-
-  useEffect(() => {
-    if (data) {
-      setHasJellyBot(data.toNumber() > 0);
-    }
-  }, [data]);
-
-  return (
-    <div>
-      {hasJellyBot ? (
-        <p>You own a JellyBot! Well done!</p>
-      ) : (
-        <p>You don&apos;t own a JellyBot yet :(</p>
-      )}
-    </div>
-  );
-};
+import Link from 'next/link';
+import abiFile from "../web3domain.json";
+var w3d = require("@web3yak/web3domain");
 
 //https://blog.0x3.studio/a-very-simple-to-offer-a-connect-wallet-option-for-your-website-thanks-to-rainbowkit/
-function DomainList() {
-
-  return (
-    <div>
-  
-I will list my domains here <hr/>
-
-    </div>
-  );
-}
 
 export default function SignExample1() {
   const { isConnected, connector, address } = useAccount();
+  const [domainAddr, setDomainAddr] = useState(null);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const settings = {
+      matic_rpc_url: "https://polygon-mainnet.g.alchemy.com/v2/K_A1JxpxF8dGzjk6MT5AhMYqXJj4zryx",
+      eth_rpc_url: "https://eth-mainnet.g.alchemy.com/v2/3xuizzW1DmQMU6Nvo0bmp297MG7BjOKl",
+      fvm_rpc_url: "https://api.node.glif.io/rpc/v1"
+    };
+    
+    
+    const resolve = new w3d.Web3Domain(settings);
+    if (address) {
+    resolve
+      .getDomainList(address)
+      .then((w3d) => {
+        setDomainAddr(w3d);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    }   
+  }, [address]);
 
   if (isConnected) {
     return (
@@ -75,7 +64,7 @@ export default function SignExample1() {
       >
         <Box
           textAlign="center"
-          alignContent={'center'}
+          alignContent={"center"}
           borderRadius="lg"
           p={{ base: 5, lg: 16 }}
           bgSize={"lg"}
@@ -95,14 +84,32 @@ export default function SignExample1() {
               <div>
                 <NextSeo title="My Domain list" />
                 <Heading as="h2" fontSize="2xl" my={4}>
-                  My Domain list 
+                  My Domain list
                 </Heading>
                 <div>Connected to {address}</div>
-                <p>
-                  These are the domains I have in my wallet
-                </p>
+                <p>These are the domains I have in my wallet</p>
 
-                <DomainList />
+                {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {error ? (
+            <p>Error: {error}</p>
+          ) : (
+            <table>
+              <tbody>
+                {domainAddr.map((item, index) => (
+                  <tr key={index}>
+                    <td> {item[0]}</td>
+                    <td><Link href={`/info/${item[1]}`}>{item[1]}</Link></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </>
+      )}
+
               </div>
             </Stack>
           </Container>
