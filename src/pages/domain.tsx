56f590b8fce5759f0171eacc3c63aaae77a2cc9c
gupta977 <navneet@odude.com>
2023-08-01 13:09:07 +0545

    final

--- src/pages/domain.tsx
+++ src/pages/domain.tsx
@@ -15,61 +15,60 @@ import {
 import { NextSeo } from "next-seo";
 import Link from 'next/link';
 var w3d = require("@web3yak/web3domain");
+type DomainTuple = [string, string];
 
 //https://blog.0x3.studio/a-very-simple-to-offer-a-connect-wallet-option-for-your-website-thanks-to-rainbowkit/
 
 export default function DomainList() {
   const { isConnected, connector, address } = useAccount();
   const { chain } = useNetwork();
-  const [domainAddr, setDomainAddr] = useState([]);
-  const [error, setError] =  useState(''); // Specify the type for error state
+  const [domainAddr, setDomainAddr] = useState<DomainTuple[]>([]);
+  const [error, setError] = useState(''); // Specify the type for error state
   const [isLoading, setIsLoading] = useState(true);
 
-
-const fetchData = () => {
-
-  const settings = {
-    matic_rpc_url: "https://polygon-mainnet.g.alchemy.com/v2/K_A1JxpxF8dGzjk6MT5AhMYqXJj4zryx",
-    eth_rpc_url: "https://eth-mainnet.g.alchemy.com/v2/3xuizzW1DmQMU6Nvo0bmp297MG7BjOKl",
-    fvm_rpc_url: "https://api.node.glif.io/rpc/v1"
-  };
-  const resolve = new w3d.Web3Domain(settings);
-
-  
-    
-
-  let provider = ''
-  if(chain)
-  {
-if(chain.network == 'filecoin-mainnet')
-{
-provider='fvm';
-console.log(provider);
-}
-  }
-
-  if (address) {
-    resolve
-      .getDomainList(address, provider)
-      .then(data => { 
-        setDomainAddr(data);
-      })
-      .catch((err) => {
-        setError(err.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }
-
-}
-
-
   useEffect(() => {
-   
+
+
+    const fetchData = () => {
+
+      const settings = {
+        matic_rpc_url: "https://polygon-mainnet.g.alchemy.com/v2/K_A1JxpxF8dGzjk6MT5AhMYqXJj4zryx",
+        eth_rpc_url: "https://eth-mainnet.g.alchemy.com/v2/3xuizzW1DmQMU6Nvo0bmp297MG7BjOKl",
+        fvm_rpc_url: "https://api.node.glif.io/rpc/v1"
+      };
+      const resolve = new w3d.Web3Domain(settings);
+
+
+
+
+      let provider = ''
+      if (chain) {
+        if (chain.network == 'filecoin-mainnet') {
+          provider = 'fvm';
+          console.log(provider);
+        }
+      }
+
+      if (address) {
+        resolve
+          .getDomainList(address, provider)
+          .then((data: DomainTuple[]) => {
+            setDomainAddr(data);
+          })
+          .catch((err: Error) => {
+            setError(err.message);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+      }
+
+    }
+
+
     fetchData();
 
-    }, [address, chain]);
+  }, [address, chain]);
 
   if (isConnected) {
     return (
@@ -105,50 +104,50 @@ console.log(provider);
                 <Heading as="h2" fontSize="2xl" my={4}>
                   My Domain list
                 </Heading>
-      
+
 
                 {isLoading ? (
-        <Spinner
-        thickness='4px'
-        speed='0.65s'
-        emptyColor='gray.200'
-        color='blue.500'
-        size='xl'
-      />
-      ) : (
-        <>
-          {error ? (
-            <p>Error: {error}</p>
-          ) : (
+                  <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                  />
+                ) : (
+                  <>
+                    {error ? (
+                      <p>Error: {error}</p>
+                    ) : (
 
-<SimpleGrid
-  bg='gray.50'
-  columns={{ sm: 2, md: 4 }}
-  spacing='8'
-  p='1'
-  textAlign='center'
-  rounded='lg'
-  color='gray.400'
->
-        
-                {domainAddr.map((item,index) => (
+                      <SimpleGrid
+                        bg='gray.50'
+                        columns={{ sm: 2, md: 4 }}
+                        spacing='8'
+                        p='1'
+                        textAlign='center'
+                        rounded='lg'
+                        color='gray.400'
+                      >
 
-<Box boxShadow='lg' p='6' rounded='md' bg='white' key={index}>
+                        {domainAddr.map((item, index) => (
 
-                 <Link href={`/info/${item[1]}`}>{item[1]}</Link>
-                  </Box>
-                ))}
-       
-              </SimpleGrid>
-          )}
-        </>
-      )}
+                          <Box boxShadow='lg' p='6' rounded='md' bg='white' key={index}>
+
+                            <Link href={`/info/${item[1]}`}>{item[1]}</Link>
+                          </Box>
+                        ))}
+
+                      </SimpleGrid>
+                    )}
+                  </>
+                )}
 
               </div>
 
-              
 
-  
+
+
 
             </Stack>
           </Container>
