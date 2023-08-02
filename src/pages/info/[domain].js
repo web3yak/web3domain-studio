c5d049154ab9d68b95013db3e55d5d55a18fb97e
gupta977 <navneet@odude.com>
2023-08-02 13:33:04 +0545

    info

--- src/pages/info/[domain].js
+++ src/pages/info/[domain].js
@@ -7,23 +7,32 @@ import {
   Button,
   Container,
   Flex,
-  FormControl,
-  FormLabel,
+  SkeletonText,
+  CardHeader,
   Heading,
   Stack,
-  Textarea,
+  SkeletonCircle,
   useColorModeValue,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Text
 } from "@chakra-ui/react";
 
 export default function Info() {
   const router = useRouter();
   const { domain } = router.query;
-
+  const [jsonData, setJsonData] = useState(null); // Initialize jsonData as null
   const [domainAddr, setDomainAddr] = useState(null);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+
+    setIsLoading(true); // Set isLoading to true whenever the effect runs
+
+
     const settings = {
       matic_rpc_url: "https://polygon-mainnet.g.alchemy.com/v2/K_A1JxpxF8dGzjk6MT5AhMYqXJj4zryx",
       eth_rpc_url: "https://eth-mainnet.g.alchemy.com/v2/3xuizzW1DmQMU6Nvo0bmp297MG7BjOKl",
@@ -31,6 +40,20 @@ export default function Info() {
     };
 
     const resolve = new w3d.Web3Domain(settings);
+
+    const url = "https://w3d.name/api/v1/index.php?domain="+domain;
+    const fetchData = async () => {
+      try {
+          const response = await fetch(url);
+          const json = await response.json();
+          setJsonData(json); // Store the json response in the component's state
+          console.log(json);
+      } catch (error) {
+          console.log("error", error);
+      }
+  };
+
+  fetchData();
 
     if (domain) {
       resolve.getAddress(domain, "ETH")
@@ -78,15 +101,70 @@ export default function Info() {
       Checking info {domain}
 
 {isLoading ? (
-  <svg /* Add your loading SVG component here */ />
+  <Box padding='6' boxShadow='lg' bg='white'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+</Box>
 ) : (
   <>
     {error ? (
       <p>Error: {error}</p>
     ) : (
       <p>
-        {domain} Domain = {domainAddr !== null ? <Link href={`/address/${domainAddr}`}>{domainAddr}</Link>  : "Invalid Web3 Domain"}
+       {domainAddr !== null ? <Link href={`/address/${domainAddr}`}>{domainAddr}</Link>  : ""}
+    
+        {domainAddr !== null ? 
+      
+        <Card
+  direction={{ base: 'column', sm: 'row' }}
+  overflow='hidden'
+  variant='outline'
+>
+  <Image
+    objectFit='cover'
+    maxW={{ base: '100%', sm: '200px' }}
+    src={jsonData?.image}
+    alt='Caffe Latte'
+  />
+
+  <Stack>
+    <CardBody>
+      <Heading size='md'>{jsonData?.name}</Heading>
+
+      <Text py='2'>
+      {jsonData?.description}
+      </Text>
+    </CardBody>
+
+    <CardFooter>
+      <Button variant='solid' colorScheme='blue'>
+        Buy Latte
+      </Button>
+    </CardFooter>
+  </Stack>
+</Card>
+    : 
+    
+    <Card align='center'>
+  <CardHeader>
+    <Heading size='md'>{domain}</Heading>
+  </CardHeader>
+  <CardBody>
+    <Text>Not available</Text>
+  </CardBody>
+  <CardFooter>
+    <Button colorScheme='blue'>Register</Button>
+  </CardFooter>
+</Card>
+    
+    
+    
+    }
+    
       </p>
+
+
+
     )}
   </>
 )}
