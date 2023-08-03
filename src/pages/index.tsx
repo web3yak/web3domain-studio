e767f57ff96490767392ed5401e6b74580cb6568
gupta977 <navneet@odude.com>
2023-08-03 12:21:27 +0545

    search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
+import React, { useEffect, useState } from 'react';
 import { useAccount, useSignMessage, useContractRead, erc721ABI, useNetwork } from "wagmi";
 import styles from "../styles/Home.module.css";
 import {
   Box,
   Input,
-  Text
+  Text,
+  Divider,
+  AbsoluteCenter
 } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const { isConnected, connector, address } = useAccount();
+  const [value, setValue] = useState('');
+  const [showBox, setShowBox] = useState(false); // Added state to control visibility of Box
+
+  const handleInputFocus = () => {
+    setShowBox(false); // Hide Box when input field is clicked
+  };
+
+  const fetchData = (param: string) => {
+    setValue(param);
+    setShowBox(true); // Show Box when value is updated
+    console.log(param);
+  };
+
   if (isConnected) {
   return (
     <div className={styles.container}>
@@ -18,7 +34,21 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Web3Domain Search
         </h1>
-           <Input placeholder='Enter domain name to search' />
+        {showBox && (
+            <Box position='relative' padding='10'>
+              <Divider />
+              <AbsoluteCenter bg='white' px='4'>
+                {value}
+              </AbsoluteCenter>
+            </Box>
+          )}
+      <Input
+        value={value}
+        onChange={(ev) => fetchData(ev.target.value)}
+        placeholder='Here is a sample placeholder'
+        onFocus={handleInputFocus} // Handle onFocus event to hide Box
+        size='sm'
+      />
 
       </main>
     </div>
