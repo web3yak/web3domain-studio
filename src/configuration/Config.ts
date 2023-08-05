882fab572ebd45ede02321506d6482c8083ba44a
gupta977 <navneet@odude.com>
2023-08-05 14:49:10 +0545

    config

--- src/configuration/Config.ts
+++ src/configuration/Config.ts
@@ -17,3 +17,8 @@ export const SOCIAL_DISCORD = 'web3yak'
 
 export const INFURA_KEY = '817597f04d6941649c41255a1b10e815'
 export const NETWORKS = [polygon, filecoin ]
+
+export const DOMAIN = 'yak' //primary domain name without dot (.)
+export const DOMAIN_PRICE_ETH = '1' //price should be equal to contract or higher 
+export const DOMAIN_IMAGE_URL = 'https://ipfs.io/ipfs/bafkreia56vpemgrsnpyisbwfwwb44ktfk2uu4rgbwn65wflbx7e6qgx5eu' //Image path starts with ipfs:// or https://
+export const DOMAIN_NETWORK_CHAIN = 'polygon' //network name where primary domain is minted
