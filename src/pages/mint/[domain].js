5a705363e4bf0607e61945dff012598acba5b00a
gupta977 <navneet@odude.com>
2023-08-09 18:45:50 +0545

    mint

--- src/pages/mint/[domain].js
+++ src/pages/mint/[domain].js
@@ -33,69 +33,88 @@ import { DOMAIN, DOMAIN_PRICE_ETH, DOMAIN_IMAGE_URL, DOMAIN_NETWORK_CHAIN } from
 
 
 const CONTRACT_ADDRESS = '0x7D853F9A29b3c317773A461ed87F54cdDa44B0e0';
-const uniqueId =  Math.round(Date.now() * Math.random()).toString();
-var claim_id = '';
-var claim_name = '';
-var claim_url ='http://yahoo.com';
-var claim_transfer_to = '';
-var amount = DOMAIN_PRICE_ETH;
+
 
 
 export default function Info() {
   const { address, connector, isConnected } = useAccount()
   const router = useRouter();
   const { domain } = router.query;
+  
+  const uniqueId = Math.round(Date.now() * Math.random()).toString();
+  const claim_id = '8888888';
+  const claim_name = 'wedddb3.yak';
+  const claim_url = 'http://yahoo.com';
+  const claim_transfer_to = '0x8D714B10B719c65B878F2Ed1436A964E11fA3271';
+  const amount = DOMAIN_PRICE_ETH;
 
- // var domain_claim = await claim(claim_id, claim_name, claim_url, claim_transfer_to, amount);
-
-
-const {
-  config,
-  error: prepareError,
-  isError: isPrepareError,
-} = usePrepareContractWrite({
-  address: CONTRACT_ADDRESS,
-  abi: abiFile.abi,
-  functionName: 'claim',
-  args: [claim_id, claim_name, claim_url, claim_transfer_to],
-  overrides: {
-    value: ethers.utils.parseEther(DOMAIN_PRICE_ETH)
-  }
-})
-const { data, error, isError, write } = useContractWrite(config)
-
-const { isLoading, isSuccess } = useWaitForTransaction({
-  hash: data?.hash,
-})
+  const [domainName, setDomainName] = useState('');
 
 
 
-  useEffect(() => {
+  // var domain_claim = await claim(claim_id, claim_name, claim_url, claim_transfer_to, amount);
+
+
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: CONTRACT_ADDRESS,
+    abi: abiFile.abi,
+    functionName: 'claim',
+    args: [claim_id, claim_name, claim_url, claim_transfer_to],
+    overrides: {
+      value: ethers.utils.parseEther(DOMAIN_PRICE_ETH)
+    }
+  })
+  const { data, error, isError, write } = useContractWrite(config)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+
+  const handleMint = async () => {
+
+    console.log("hello " + domain);
     claim_name = domain;
     claim_id = uniqueId;
     claim_transfer_to = address;
 
+    if (!isPrepareError) {
+      write();
+    }
+    else {
+      console.log("prepare error not working");
+    }
+
+  }
+
+
+  useEffect(() => {
+
     console.log(DOMAIN_PRICE_ETH);
     console.log(claim_name);
-   console.log(claim_id);
-   console.log(claim_transfer_to);
+    console.log(claim_id);
+    console.log(claim_transfer_to);
 
 
-  }, [domain]);
+  }, [domain, address]);
 
   return (
-<>
-Domain from URL: {domain}
-<br/>
-Claim ID{claim_id}
-<br/>
-Claim name: {claim_name}
-<br/>
-Transfer to: {claim_transfer_to}
-<hr>
-</hr>
+    <>
+      Domain from URL: {domain}
+      <br />
+      Claim ID{claim_id}
+      <br />
+      Claim name: {claim_name}
+      <br />
+      Transfer to: {claim_transfer_to}
+      <hr>
+      </hr>
 
-      <button disabled={!write || isLoading}  onClick={() => write()}>
+      <button onClick={() => handleMint()}>
         {isLoading ? 'Minting...' : 'Mint'}
       </button>
       {isSuccess && (
@@ -106,10 +125,14 @@ Transfer to: {claim_transfer_to}
           </div>
         </div>
       )}
-      {(isPrepareError || isError) && (
-        <div>Error: {(prepareError || error)?.message}</div>
+      {(isPrepareError) && (
+        <div>PrepareError: {(prepareError || error)?.message}</div>
       )}
-</>
+
+      {(isError) && (
+        <div>isError: {(prepareError || error)?.message}</div>
+      )}
+    </>
 
   );
 }
