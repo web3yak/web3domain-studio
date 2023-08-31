1f70e2b2e7d239923a9178760cb6f523d9b7e17d
ODude Network <web3yak@gmail.com>
2023-08-31 21:02:40 +0545

    image

--- src/pages/domain/host/[host].js
+++ src/pages/domain/host/[host].js
@@ -6,6 +6,9 @@ import useDomainInfo from '../../../hooks/domainInfo';
 import { useURLValidation } from '../../../hooks/validate';
 import { useNetworkValidation, checkContract } from '../../../hooks/useNetworkValidation';
 import { useJsonValue } from "../../../hooks/jsonData";
+import { generateJson, generateImage } from '../../../hooks/ipfs';
+import TokenURI from '../../../components/TokenURI'; // Adjust the path to the actual location
+
 import {
   Box,
   Button,
@@ -46,7 +49,7 @@ import {
 } from '@chakra-ui/react'
 import { FaCopy, FaExternalLinkAlt, FaForward } from "react-icons/fa";
 import { useAccount, useNetwork } from "wagmi";
-import {  DOMAIN_TLD, NETWORK_ERROR } from '../../../configuration/Config'
+import { DOMAIN_TLD, NETWORK_ERROR } from '../../../configuration/Config'
 
 
 export default function Info() {
@@ -60,7 +63,7 @@ export default function Info() {
   const [jsonData, setJsonData] = useState(null); // Initialize jsonData as null
   const { getValue } = useJsonValue(jsonData);
   const [error, setError] = useState('');
-
+  const [claimUrl, setClaimUrl] = useState('http://web3domain.org');
   const [isLoading, setIsLoading] = useState(true);
 
   const [flag, setFlag] = useBoolean();
@@ -80,37 +83,64 @@ export default function Info() {
 
     console.log(jsonData);
 
-        // Update the jsonDataNew object with the new web3_url value
-        const updatedJsonData = {
-          ...jsonData,
-          records: {
-            ...jsonData.records,
-            "51": {
-              type: "web3_url",
-              value: newUrl
-            }
-          }
-        };
-    
-        setJsonDataNew(updatedJsonData); // Update the state with the modified jsonData
-    
-        console.log(updatedJsonData);
+    // Update the jsonDataNew object with the new web3_url value
+    const updatedJsonData = {
+      ...jsonData,
+      records: {
+        ...jsonData.records,
+        "51": {
+          type: "web3_url",
+          value: newUrl
+        }
+      }
+    };
+
+    setJsonDataNew(updatedJsonData); // Update the state with the modified jsonData
+
+    console.log(updatedJsonData);
   };
+
+  const handleUpload = async () => {
+    console.log("Verify record of  " + domain);
+    setIsLoading(true);
+    if (domain !== 'undefined') {
+
+      console.log(jsonData);
+
+      await genJson();
+
+    }
+  }
+
+  async function genJson() {
+    //handleSubmit(null); 
+    console.log(jsonDataNew);
+    const response = await generateJson(jsonDataNew, domain);
+    if (response.ok) {
+      const responseText = await response.text();
+
+      try {
+        const responseObject = JSON.parse(responseText);
+        const cidValue = responseObject.cid;
+        console.log('https://ipfs.io/ipfs/' + cidValue);
+        setClaimUrl('https://ipfs.io/ipfs/' + cidValue);
+        setIsLoading(false);
+
+
+      } catch (error) {
+        console.log("Error parsing JSON:", error);
+      }
+
+    } else {
+      console.log("Error generating JSON.");
+      setIsLoading(false);
+    }
+
+  }
 
   useEffect(() => {
 
     setIsLoading(true); // Set isLoading to true whenever the effect runs
-    const settings = {
-      matic_rpc_url: process.env.NEXT_PUBLIC_MATIC,
-      eth_rpc_url: process.env.NEXT_PUBLIC_ETH,
-      fvm_rpc_url: process.env.NEXT_PUBLIC_FILECOIN
-    };
-
-    const resolve = new w3d.Web3Domain(settings);
-
-    // console.log(resolve.SmartContractAddress); //Polygon Mainnet contract address
-    // console.log(resolve.fvm_SmartContractAddress);  //Filecoin 
-
 
     if (domain) {
       const randomNumber = Math.random();
@@ -133,45 +163,44 @@ export default function Info() {
     }
   }, [domain]);
 
+  useEffect(() => {
+    console.log(visitUrl);
+  }, [visitUrl]);
+
+
   // Use another useEffect to set webUrl
   useEffect(() => {
 
     console.log(jsonData);
     if (jsonData) {
-    var web2_url =getValue("web_url");
-    //console.log(web2_url);
+      var web2_url = getValue("web_url");
+      //console.log(web2_url);
 
-    var web3_url =getValue("web3_url");
-    //console.log(web3_url);
+      var web3_url = getValue("web3_url");
+      //console.log(web3_url);
 
 
-    setWeb2Url(web2_url);
-    setWeb3Url(web3_url);
+      setWeb2Url(web2_url);
+      setWeb3Url(web3_url);
 
-    console.log(web3Url);
-    console.log(web2Url);
-   
+      console.log(web3Url);
+      console.log(web2Url);
 
-    const isValid = validateURL(web3Url);
- 
-    if(isValid)
-    {
-      console.log("Valid URL "+web3Url);
-      setVisitUrl(web3Url);
-    }
-    else
-    {
-      if(validateURL(web2Url))
-      {
-      setVisitUrl(web2Url);
+
+      const isValid = validateURL(web3Url);
+
+      if (isValid) {
+        console.log("Valid URL " + web3Url);
+        setVisitUrl(web3Url);
       }
-      else
-      {
-        setVisitUrl("#");
+      else {
+        if (validateURL(web2Url)) {
+          setVisitUrl(web2Url);
+        }
+
       }
+      console.log(visitUrl);
     }
-    console.log(visitUrl);
-  }
 
   }, [jsonData]);
 
@@ -200,147 +229,160 @@ export default function Info() {
         >
           <Kbd><Link href={`/domain/info/${domain}`}>{domain}</Link></Kbd>
           <Box
-          textAlign="center"
-          alignContent={"center"}
-          borderRadius="lg"
-          p={{ base: 5, lg: 2 }}
-          bgSize={"lg"}
-          maxH={"80vh"}
-        >
-          {isNetworkValid && domain.endsWith('.' + DOMAIN_TLD) ? (
-          <Stack
-            as={Box}
-            textAlign={"center"}
-            spacing={{ base: 2, md: 2 }}
-            py={{ base: 10, md: 6 }}
+            textAlign="center"
+            alignContent={"center"}
+            borderRadius="lg"
+            p={{ base: 5, lg: 2 }}
+            bgSize={"lg"}
+            maxH={"80vh"}
           >
+            {isNetworkValid && domain.endsWith('.' + DOMAIN_TLD) ? (
+              <Stack
+                as={Box}
+                textAlign={"center"}
+                spacing={{ base: 2, md: 2 }}
+                py={{ base: 10, md: 6 }}
+              >
 
-            {isLoading ? (
-              <Box padding='6' boxShadow='lg' bg='white'>
-                <SkeletonCircle size='10' />
-                <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='3' />
-              </Box>
-            ) : (
-              <>
-                {error ? (
-                  <p>Error: {error}</p>
+                {isLoading ? (
+                  <Box padding='6' boxShadow='lg' bg='white'>
+                    <SkeletonCircle size='10' />
+                    <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='3' />
+                  </Box>
                 ) : (
-                  <p>
+                  <>
+                    {error ? (
+                      <p>Error: {error}</p>
+                    ) : (
+                      <p>
 
-                    {address == ownerAddress  ?
-           <form onSubmit={handleSubmit}>
-                      <Card
-                        direction={{ base: 'column', sm: 'row' }}
-                        overflow='hidden'
-                        variant='outline'
-                        align='center'
-                      >
+                        {address == ownerAddress ?
+                          <form onSubmit={handleSubmit}>
+                            <Card
+                              direction={{ base: 'column', sm: 'row' }}
+                              overflow='hidden'
+                              variant='outline'
+                              align='center'
+                            >
 
-                        <Image
-                          ml={2}
-                          boxSize='150px'
-                          src={jsonData?.image && jsonData.image.startsWith("ipfs://") ? jsonData.image.replace("ipfs://", "https://ipfs.io/ipfs/") : jsonData?.image}
-                          alt={jsonData?.name}
-                        />
+                              <Image
+                                ml={2}
+                                boxSize='150px'
+                                src={jsonData?.image && jsonData.image.startsWith("ipfs://") ? jsonData.image.replace("ipfs://", "https://ipfs.io/ipfs/") : jsonData?.image}
+                                alt={jsonData?.name}
+                              />
 
-                        <Stack>
-                          <CardBody>
-                         
-                          <Text mb='4px'>Redirect to:</Text>
-                                <InputGroup>
+                              <Stack>
+                                <CardBody>
 
-                                  <Input
-                                    value={visitUrl}
-                                    placeholder='No website defined!'
-                                    size='sm'
-                                    disabled="true" 
-                                  />
-                                  {web3Url != null && (
-                                    <InputRightElement width='1rem' >
+                                  <Text mb='4px'>Redirect to:</Text>
+                                  <InputGroup>
 
-                                      <Link href={`${visitUrl}`} passHref>
-                                        <a target="_blank" rel="noopener noreferrer">
-                                          <FaExternalLinkAlt mx='2px' />
-                                        </a>
-                                      </Link>
-                                    </InputRightElement>
-                                  )}
-                                </InputGroup>
-                                <br />
-
-                                <FormControl display='flex' alignItems='center'>
-                                  <FormLabel htmlFor='change-url' mb='0'>
-                                    Turn on Redirects to own link
-                                  </FormLabel>
-                                  <Switch id='change-url' onChange={() => {
-                                    setFlag.toggle();
-                                    //handleFlagChange();
-                                  }} isChecked={flag} />
-                                </FormControl>
-
-                                {flag && (
-                                  <FormControl mt={2}>
-                                    <FormLabel>Your New Website URL</FormLabel>
                                     <Input
-                                      type="url"
-                                      placeholder="http://"
-                                      size="md"
-                                      value={newUrl}
-                                      onChange={(event) =>
-                                        setNewUrl(event.currentTarget.value)
-                                      }
+                                      value={visitUrl}
+                                      placeholder='No website defined!'
+                                      size='sm'
+                                      disabled="true"
                                     />
-                                    <FormHelperText>
-                                      IPFS & http URL both are supported.<br/>
-                                      {newUrl}
-                                    </FormHelperText>
+                                    {web3Url != null && (
+                                      <InputRightElement width='1rem' >
+
+                                        <Link href={`${visitUrl}`} passHref>
+                                          <a target="_blank" rel="noopener noreferrer">
+                                            <FaExternalLinkAlt mx='2px' />
+                                          </a>
+                                        </Link>
+                                      </InputRightElement>
+                                    )}
+                                  </InputGroup>
+                                  <br />
+
+                                  <FormControl display='flex' alignItems='center'>
+                                    <FormLabel htmlFor='change-url' mb='0'>
+                                      Turn on Redirects to own link
+                                    </FormLabel>
+                                    <Switch id='change-url' onChange={() => {
+                                      setFlag.toggle();
+                                      //handleFlagChange();
+                                    }} isChecked={flag} />
                                   </FormControl>
-                                )}
 
-                          </CardBody>
+                                  {flag && (
+                                    <FormControl mt={2}>
+                                      <FormLabel>Your New Website URL</FormLabel>
+                                      <Input
+                                        type="url"
+                                        placeholder="http://"
+                                        size="md"
+                                        value={newUrl}
+                                        onChange={(event) =>
+                                          setNewUrl(event.currentTarget.value)
+                                        }
+                                      />
+                                      <FormHelperText>
+                                        IPFS & http URL both are supported.<br />
+                                        {newUrl}
+                                      </FormHelperText>
+                                    </FormControl>
+                                  )}
 
-                          <CardFooter>
-                            {address == ownerAddress ? (
-                              <div>
-                                <Button rightIcon={<FaForward />} colorScheme="teal" type="submit" width="half" mt={4}>
-                              Save
-                            </Button>
-                                &nbsp;
-                                <Button variant='solid' colorScheme='yellow'>
-                                  <Link href={`/domain/manage/${domain}`}>Update Website</Link>
-                                </Button>
+                                </CardBody>
 
-                                &nbsp;
-                              </div>
-                            ) : (<>Not authorized</>)}
+                                <CardFooter>
+                                  {address == ownerAddress ? (
+                                    <div>
+                                      <Button rightIcon={<FaForward />} colorScheme="teal" type="submit" width="half" mt={4}>
+                                        Save
+                                      </Button>
+                                      &nbsp;
+                                      {jsonDataNew != null ? (
+                                        <Button rightIcon={<FaForward />} colorScheme="green" width="half" mt={4} onClick={() => handleUpload()} >
 
-                 
-                          </CardFooter>
-                        </Stack>
-                      </Card>
-                      </form>
-                      :
+                                          {isLoading ? (
 
-                      <Alert status='error'>
-                      <AlertIcon />
-                      <AlertTitle>You are not authorized.</AlertTitle>
-                    </Alert>
+                                            <>  <CircularProgress isIndeterminate size="24px" /> Submitting </>
+                                          ) : (
+                                            'Verify'
+                                          )}
 
-                    }
+                                        </Button>
+                                      ) : (
+                                        <></>
+                                      )}
 
-                  </p>
+                                      &nbsp;
+                                      {claimUrl != 'http://web3domain.org' ? (<TokenURI domainName={domain} TokenURI={claimUrl} />) : (<></>)}
 
+                                    </div>
+                                  ) : (<>Not authorized</>)}
 
 
+                                </CardFooter>
+                              </Stack>
+                            </Card>
+                          </form>
+                          :
+
+                          <Alert status='error'>
+                            <AlertIcon />
+                            <AlertTitle>You are not authorized.</AlertTitle>
+                          </Alert>
+
+                        }
+
+                      </p>
+
+
+
+                    )}
+                  </>
                 )}
-              </>
-            )}
 
-          </Stack>
-           ) :
-           (<>{NETWORK_ERROR}</>)
-         }
-         </Box>
+              </Stack>
+            ) :
+              (<>{NETWORK_ERROR}</>)
+            }
+          </Box>
         </Container>
 
       </Box>
