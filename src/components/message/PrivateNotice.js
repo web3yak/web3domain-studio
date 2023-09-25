5fd5d80b3282e4d506448522c8508d5b3b0029bd
ODude Network <web3yak@gmail.com>
2023-09-25 20:38:25 +0545

    notice

--- src/components/message/PrivateNotice.js
+++ src/components/message/PrivateNotice.js
@@ -30,13 +30,32 @@ import {
   Lorem,
 } from "@chakra-ui/react";
 
-import jData from './PrivateNotice.json';
+import jData from "./PrivateNotice.json";
 
 export default function privateNotice() {
   const { address } = useAccount();
-  const [status, setStatus] = useState("none");
+  const [status, setStatus] = useState("");
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [noticeData, setNoticeData] = useState([]);
+  const [modify, setModify] = useState(false);
+
+  const edit = () => {
+    console.log("Modify");
+    setModify(true);
+  };
+
+  const update = () => {
+    console.log("Update");
+
+    setModify(false);
+    // setModify(true);
+  };
+
+  const closeModal = () => {
+    console.log("Close the modal");
+    setModify(false); // Set modify to false when the modal is closed
+    onClose(); // Close the modal
+  };
 
   useEffect(() => {
     if (address) {
@@ -53,54 +72,64 @@ export default function privateNotice() {
     // Fetch and set the JSON data (you can use an API request or import it as in this example)
     console.log(jData); // Add this line for debugging
     setNoticeData(jData);
-  }, [noticeData]); 
+  }, [noticeData]);
 
+  useEffect(() => {
+    // Fetch and set the JSON data (you can use an API request or import it as in this example)
+    console.log(modify); // Add this line for debugging
+    // setNoticeData(jData);
+  }, [modify]);
 
   return (
     <div>
       <Button onClick={onOpen}>Bulletin board</Button>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={closeModal}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Bulletin board</ModalHeader>
+          <ModalHeader>
+            Bulletin board{" "}
+            {!modify ? <Button onClick={() => edit()}>Edit</Button> : null}
+          </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-          {status == "GOLD" ?(
-          <ul>
-        {noticeData.map((notice) => (
-          <li key={notice.ID}>
-            ID is {notice.ID}, Date is {notice.Date}, Title is {notice.Title}, Message is {notice.Message}
-          </li>
-        ))}
-      </ul>
-          )
-          :
-          (
-            <>You are not a member</>
-          )
-
-        }
-
-
-
+            {status == "GOLD" ? (
+              <>
+                {!modify ? (
+                  <ul>
+                    {noticeData.map((notice) => (
+                      <li key={notice.ID}>
+                        ID is {notice.ID}, Date is {notice.Date}, Title is{" "}
+                        {notice.Title}, Message is {notice.Message}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <>Modify It </>
+                )}
+              </>
+            ) : (
+              <>You are not a member</>
+            )}
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            {modify ? <Button onClick={() => update()}>Update</Button> : null}
+
+            <Button colorScheme="blue" mr={3} onClick={closeModal}>
               Close
             </Button>
+
             {status != "GOLD" && (
-             <Link href="/list">
-             <Button colorScheme="teal" variant="solid">
-               Check Domain
-             </Button>
-             </Link>
+              <Link href="/list">
+                <Button colorScheme="teal" variant="solid">
+                  Check Domain
+                </Button>
+              </Link>
             )}
           </ModalFooter>
         </ModalContent>
       </Modal>
-
     </div>
   );
 }
