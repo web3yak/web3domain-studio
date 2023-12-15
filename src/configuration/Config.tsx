b2d67b6e41b5dcaf20ea28bd7d52011150f80322
Hi0x <hi@hi0x.net>
2023-12-15 14:49:21 +0200

    Update Config.tsx

--- src/configuration/Config.tsx
+++ src/configuration/Config.tsx
@@ -18,8 +18,8 @@ export const SOCIAL_DISCORD = ''
 export const NETWORKS = [polygon]; //polygon, filecoin, polygonMumbai
 export const NETWORK_ERROR = "Unsupported Blockchain Network or Domain Name !" //Change network name as required
 
-export const DOMAIN_TLD = 'yak' //primary domain name without dot (.)
-export const DOMAIN_PRICE_ETH = '0.1' //price should be equal to contract or higher 
+export const DOMAIN_TLD = '0x' //primary domain name without dot (.)
+export const DOMAIN_PRICE_ETH = '1' //price should be equal to contract or higher 
 export const DOMAIN_IMAGE_URL = 'https://web3domain.org/api/nft/yak.jpg' //Image path starts with ipfs:// or https://
 export const DOMAIN_NETWORK_CHAIN = 137 //137 for polygon, 314 for filecoin, 80001 form mumbai
 export const DOMAIN_DESCRIPTION = 'My Domain description goes here....'
@@ -27,9 +27,9 @@ export const DOMAIN_TYPE = "W3D" //W3D for polygon, FVM for Filecoin net
 export const DOMAIN_TITLE = "Web3 Domain Search" //Title above the search input field. 
 export const DOMAIN_PLACEHOLDER = "Search for a name" //Placeholder for search input field 
 
-export const ADMIN_WALLET = "0x8D714B10B719c65B878F2Ed1436A964E11fA3271" //ETH wallet address 
+export const ADMIN_WALLET = "0x6ec0Ce3333A7A2a37e38c656B46752F2a9fC8e19" //ETH wallet address 
 
 export const NOTICE_TITLE = "Bulletin board"
-export const NOTICE_NON_MEMBER = "Only the .yak domain holder can view bulletin board."
+export const NOTICE_NON_MEMBER = "Only the .0x domain holder can view bulletin board."
 
 export const DOMAIN_BANNER = "https://web3domain.org/studio/wp-content/uploads/2023/06/a6.jpg" //290x80 size
